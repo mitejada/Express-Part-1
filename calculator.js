20517d704c20bd0952d2ccba435c8f4c0f56d60f
Michell Tejada <michelltejada@pursuit.com>
2018-12-03 21:22:06 -0500

    Calculator One Route

--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const app = express()
 
 
-app.get('/add/:num1/:num2', (req, res) => {
+app.get('/:operator/:num1/:num2', (req, res) => {
   let num1 = parseInt(req.params.num1)
   let num2 = parseInt(req.params.num2)
 
@@ -10,42 +10,20 @@ app.get('/add/:num1/:num2', (req, res) => {
     res.send('Please type a number!')
   }
 
-  res.send({num1: num1, num2: num2, result: num1 + num2})
-})
-
-app.get('/subtract/:num1/:num2', (req, res) => {
-  let num1 = parseInt(req.params.num1)
-  let num2 = parseInt(req.params.num2)
-
-  if(isNaN(num1) || isNaN(num2)) {
-    res.send('Please type a number!')
+  let operator = req.params.operator
+  if(operator === '+') {
+    res.send({num1: num1, num2: num2, result: num1 + num2})
+  }else if(operator === '-'){
+    res.send({num1: num1, num2: num2, result: num1 - num2})
+  }else if(operator === '*') {
+    res.send({num1: num1, num2: num2, result: num1 * num2})
+  }else if(operator === '/') {
+    res.send({num1: num1, num2: num2, result: num1 / num2})
+  }else {
+    res.json('operator not valid')
   }
 
-  res.send({num1: num1, num2: num2, result: num1 - num2})
 })
-
-app.get('/multiply/:num1/:num2', (req, res) => {
-  let num1 = parseInt(req.params.num1)
-  let num2 = parseInt(req.params.num2)
-
-  if(isNaN(num1) || isNaN(num2)) {
-    res.send('Please type a number!')
-  }
-
-  res.send({num1: num1, num2: num2, result: num1 * num2})
-})
-
-app.get('/divide/:num1/:num2', (req, res) => {
-  let num1 = parseInt(req.params.num1)
-  let num2 = parseInt(req.params.num2)
-
-  if(isNaN(num1) || isNaN(num2)) {
-    res.send('Please type a number!')
-  }
-  
-  res.send({num1: num1, num2: num2, result: num1 / num2})
-})
-
 
 app.listen(3000, () => {
   console.log('You are listening to port 3000');
